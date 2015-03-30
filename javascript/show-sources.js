fc6e660e92efa371be59970b51713838a9e2256f
Tobias Schneider <schneider@synyx.de>
2015-03-30 20:39:06 +0200

    fix click on output close

--- javascript/show-sources.js
+++ javascript/show-sources.js
@@ -1,15 +1,19 @@
 $(function () {
     $('.code').click(function (event) {
         event.stopPropagation();
+
         var parentLoaderElement = $(this).siblings().first();
         var name = parentLoaderElement.attr('class');
+
         $('#output-html').html(parentLoaderElement[0].outerHTML);
         $('#output-css').load('stylesheets/animations/' + name + '.css');
         $('#output-header').html(name);
-        $('#output').show()
+        $('#output').show();
     });
 
-    $(':not(.code)').click(function () {
-        $('#output').hide()
+    $(document).click(function (event) {
+        if (!$(event.target).closest('#output').length) {
+            $('#output').hide();
+        }
     });
 });
